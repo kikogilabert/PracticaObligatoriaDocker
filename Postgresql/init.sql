d41b205d24f11b1adf0cd20ca644305331725b52
Alberto <albertogom.alon@gmail.com>
2024-02-12 20:06:58 +0100

    change v.2

--- Postgresql/init.sql
+++ Postgresql/init.sql
@@ -1,11 +1,31 @@
 CREATE DATABASE rent_agency;
 \c rent_agency;
 
-CREATE TABLE aparment (
+CREATE TABLE cities_city (
+    id SERIAL PRIMARY KEY,
+    slug VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    state VARCHAR(255) NOT NULL,
+    country VARCHAR(255) NOT NULL,
+    image TEXT NOT NULL
+);
+
+CREATE TABLE cities_zone (
+    id SERIAL PRIMARY KEY,
+    slug VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    zone_type VARCHAR(255) NOT NULL,
+    zone_image VARCHAR(255) NOT NULL,
+    city_id INT NOT NULL
+);
+
+ALTER TABLE cities_zone ADD CONSTRAINT fk_zone_city FOREIGN KEY (city_id) REFERENCES cities_city(id);
+
+CREATE TABLE cities_apartment (
     id SERIAL PRIMARY KEY,
     slug VARCHAR(30) NOT NULL,
-    name VARCHAR(100) NULL,
-    location VARCHAR(100) NOT NULL,
+    name VARCHAR(255) NULL,
+    location VARCHAR(255) NOT NULL,
     price INT NOT NULL,
     rooms INT NOT NULL,
     bathrooms INT NOT NULL,
@@ -14,30 +34,4 @@ CREATE TABLE aparment (
     zone_id INT NOT NULL
 );
 
-ALTER TABLE aparment ADD PRIMARY KEY (id);
-ALTER TABLE aparment ADD CONSTRAINT fk_aparment_zone FOREIGN KEY (zone_id) REFERENCES zone(id);
-
-CREATE TABLE zone (
-    id SERIAL PRIMARY KEY,
-    slug VARCHAR(100) NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    zone_type VARCHAR(100) NOT NULL,
-    location VARCHAR(100) NOT NULL,
-    zone_image TEXT NOT NULL,
-    city_id INT NOT NULL
-);
-
-ALTER TABLE zone ADD PRIMARY KEY (id);
-
-ALTER TABLE zone ADD CONSTRAINT fk_zone_city FOREIGN KEY (city_id) REFERENCES city(id);
-
-CREATE TABLE city (
-    id SERIAL PRIMARY KEY,
-    slug VARCHAR(100) NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    state VARCHAR(100) NOT NULL,
-    country VARCHAR(100) NOT NULL,
-    image TEXT NOT NULL
-);
-
-ALTER TABLE city ADD PRIMARY KEY (id);
+ALTER TABLE cities_apartment ADD CONSTRAINT fk_aparment_zone FOREIGN KEY (zone_id) REFERENCES cities_zone(id);
