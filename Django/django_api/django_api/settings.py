583c8540b25cad710f85d860bd6f3c02894b1791
Kiko Gilabert <kiko.gilabertm@gmail.com>
2024-02-16 17:10:54 +0100

    load_balancer finished

--- Django/django_api/django_api/settings.py
+++ Django/django_api/django_api/settings.py
@@ -102,6 +102,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ALLOWED_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-]
+    "http://localhost",
+    "http://localhost:80",
+    ]
